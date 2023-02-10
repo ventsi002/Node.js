c4e6465772188a988e598f4d6068d6b37aa9678b
ventsi002 <viliev2002@gmail.com>
2023-02-10 09:09:37 +0100

    variables 2

---
+++ 00._Miscellaneous/variables_II.js
@@ -0,0 +1,30 @@
+"use strict"
+
+// {
+//     var someValue = true;
+//     {
+//         var someValue = false;
+//     }
+//     console.log(someValue);
+// }
+
+// {
+//     let someValue = true;
+//     {
+//         let someValue = false;
+//     }
+//     confirm.log(someValue);
+// }
+
+// for (var i = 0; i <= 5; i++) {
+//     setTimeout(() => {
+//         console.log(i);
+//     }, 1000);
+// }
+
+
+for (let i = 0; i <= 5; i++) {
+    setTimeout(() => {
+        console.log(i);
+    }, 1000);
+}
