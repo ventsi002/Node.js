b21a5cab4556c0f5d2e5371035a9894e55d15365
ventsi002 <viliev2002@gmail.com>
2023-02-16 21:45:56 +0100

    corrected homework

--- Homework/01._Birds_API_1/app.js
+++ Homework/01._Birds_API_1/app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
 
+const birds = [
+    { id: 1, name: "parrot", color: "green" },
+    { id: 2, name: "flamingo", color: "pink" },
+    { id: 3, name: "owl", color: "brown" }
+];
+
 app.get("/birds", (req, res) => {
-    res.send({ message: "That are all birds" })
+    res.send(birds)
 });
 
-app.get("/birds/:name", (req, res) => {
-    req.query;
-    res.send({ name: req.params.name });
+app.get("/birds/:id", (req, res) => {
+    const { id } = req.params;
+    res.send(birds.find(b => b.id == id));
 });
 
 app.listen(8080);
