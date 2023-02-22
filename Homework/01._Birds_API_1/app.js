add6358215aa7404fd766fc5e4712c5d0f114c34
ventsi002 <viliev2002@gmail.com>
2023-02-23 00:30:22 +0100

    finished Birds REST 2

--- Homework/01._Birds_API_1/app.js
+++ Homework/01._Birds_API_1/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 const birds = [
     { id: 1, name: "parrot", color: "green" },
@@ -15,5 +16,39 @@ app.get("/birds/:id", (req, res) => {
     const { id } = req.params;
     res.send(birds.find(b => b.id == id));
 });
+
+app.post('/birds', (req, res) => {
+    const bird = req.body;
+    let id;
+    if (birds.length > 0) {
+        id = birds[birds.length - 1].id + 1;
+    } else {
+        id = 1;
+    }
+    const newBird = { id: id, ...bird };
+    birds.push(newBird);
+    res.send(newBird);
+});
+
+app.put('/birds/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const findBird = birds.findIndex(bird => bird.id === id);
+    if (findBird >= 0) {
+        const bird = req.body;
+        const updateBird = { id, ...bird };
+        birds[findBird] = updateBird;
+        res.send(updateBird);
+    }
+    else {
+        res.send({ message: "No such bird" });
+    }
+});
+
+app.delete('/birds/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const findBird = birds.findIndex(bird => bird.id === id);
+    birds.splice(findBird, 1);
+    res.send({ message: "Bird removed" });
+})
 
 app.listen(8080);
