65f2f27d07ab4a8b33278c65d33e8bf73e5da997
ventsi002 <viliev2002@gmail.com>
2023-03-02 21:51:09 +0100

    added 02._Birthday, homework with time

---
+++ Homework/02._Birthday/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.get("/birthday", (req, res) => {
+    res.sendFile(__dirname + "/birthday.html");
+});
+
+
+app.get("/api/birthday", (req, res) => {
+    res.send({ time: Date() });
+});
+
+
+app.listen(8080);
