5bab06c38b52c8d7098210c02164d43bf1c7d318
ventsi002 <viliev2002@gmail.com>
2023-03-03 08:23:16 +0100

    tried debugging

--- Homework/02._Birthday/app.js
+++ Homework/02._Birthday/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.get("/b", (req, res) => {
+app.get("/birthday", (req, res) => {
     res.sendFile(__dirname + "public/birthday.html");
 });
 
